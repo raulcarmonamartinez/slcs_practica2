ee945e1ecb97703ffedebd18b8b0497725d6f47c
i62camar <i62camar@uco.es>
2015-06-05 21:35:02 +0200

    Modif tras bajar el proyecto de GitHub

--- holamundo.c
+++ holamundo.c
@@ -3,6 +3,7 @@
 int main(){
 char c[100]= "Hola mundo";
 printf("%s\n", strcat(c,", soy i62camar"));
+printf("%s\n", strcat(c,", y esto es una modificación tras bajar el proyecto de GitHub"));
 return 0;
 }
 
